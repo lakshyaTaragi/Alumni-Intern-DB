6ee30a5f14b3f0337865b18bf89bcd65cf480b7b
Abhishek Yadav <abhishekyadav7121@gmail.com>
2021-03-22 01:42:02 +0530

    Header done

--- view/src/components/Home/Home.js
+++ view/src/components/Home/Home.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import './Home.css';
 import Navbar from '../Navbar/Navbar'
-import Bg from './Assets/Vector 5.svg';
-// import search from './Assets/search.svg';
+// import Bg from './Assets/Vector 5.svg';
+import search from './Assets/search.svg';
 
 class Home extends React.Component{
     render(){
         return(
-            <div className="Homecontainer">
-                {/* <img src={Bg} alt="Bg" />     */}
+            <div className="whole">
                 <Navbar/>    
+                <div className="content">
+                    <div className="desc">
+                        <h1 id="heading1">Search</h1>
+                        <h1 id="heading2">the database</h1>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo,</p>
+                         <div id="input" className="ui icon input">
+                            <input type="text" placeholder="Search..." />
+                            <i className="search icon"></i>
+                            <div class="ui selection dropdown">
+                                <input type="hidden" name="gender" />
+                                <i class="dropdown icon"></i>
+                                <div class="default text">Choose</div>
+                                <div class="menu">
+                                    <div class="item" data-value="1">Alum</div>
+                                    <div class="item" data-value="0">Intern</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={search} alt="logo" />
+                    </div>
+                    
+                </div>
             </div>
             
         )
--- view/src/components/Navbar/Navbar.js
+++ view/src/components/Navbar/Navbar.js
@@ -4,28 +4,31 @@ import './Navbar.css';
 class Navbar extends React.Component {
     render(){
         return (
-            <div className="container">
-                <div className="ui secondary menu">
-                    <a className="active item">
-                        <h3 className="main">Alumni and intern Database</h3>
-                    </a>
-                    <div className="right menu">
-                        <a className="ui item">
-                            <span className="Search">Search</span>
+            <div>
+                <div className="contain">
+                    <div className="ui secondary menu">
+                        <a className="active item">
+                            <h3 className="main">Alumni and intern Database</h3>
                         </a>
-                        <a className="ui item">
-                            Intern Form
-                        </a>
-                        <a className="ui item">
-                            Alumni Form
-                        </a>
-                        <a id="login" className="ui item">
-                            Log In
-                        </a>
+                        <div className="right menu">
+                            <a className="ui item">
+                                <span className="Search">Search</span>
+                            </a>
+                            <a className="ui item">
+                                Intern Form
+                            </a>
+                            <a className="ui item">
+                                Alumni Form
+                            </a>
+                            <a id="login" className="ui item">
+                                Log In
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
+            
         );
     }
 }
